e4393d0e6fc9059b1a119f050f509b4a94f49f3b
marvelousvenom <marvelousvenom779@gmail.com>
2025-07-02 21:29:58 +0530

    Fix deployment issue: updated base path, index.html script

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-// vite.config.ts
+// vite.config.ts or vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DAG_Builder_React/',
 });
